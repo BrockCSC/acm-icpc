9e47611c846cef654f8a969fb4b1175e50b470a9
Dennis Ideler <ideler.dennis@gmail.com>
2012-09-22 00:49:59 -0400

    Remove junk

--- tryouts-2011/jt11li/Node.java
+++
@@ -1,8 +0,0 @@
-public class Node
-{
-  public int number, colorCost;
-  public Node fatherNode;
-  public Node[] childNodes; 
-  //this is if they have all son nodes are colored 
-  public boolean isActive = true, isColored = false; 
-}
