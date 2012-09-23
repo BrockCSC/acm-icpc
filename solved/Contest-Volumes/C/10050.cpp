551a25620cec34290588ac713f2d335f58197510
Dennis Ideler <ideler.dennis@gmail.com>
2012-09-22 20:40:54 -0400

    Retab

--- solved/Contest-Volumes/C/10050.cpp
+++ solved/Contest-Volumes/C/10050.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 int main(void)
 {
-	int t; // number of test cases
-	int n; // number of days (7 <= n <= 3650)
-	int p; // number of political parties (1 <= p <= 100)
-	int hi; // hartel parameter
-	int strikedays;
-	int * harteldays; // days of strike (Fri and Sat not included)
+  int t; // number of test cases
+  int n; // number of days (7 <= n <= 3650)
+  int p; // number of political parties (1 <= p <= 100)
+  int hi; // hartel parameter
+  int strikedays;
+  int * harteldays; // days of strike (Fri and Sat not included)
 
-	cin >> t;
-	if (t <= 0) exit(0);
-	
-	for (int i = 0; i < t; i++)
-	{
-		cin >> n;
-		cin >> p;	
-		harteldays = new int[n];
-		for (int j = 0; j < p; j++)
-		{
-			cin >> hi;
-			for (int k = 0; k < n; k++)
-			{
-				if ((k+1) % hi == 0)
-				{
-					if ((k+1) % 7 != 0 && (k+2) % 7 != 0)
-						harteldays[k] = 1;
-					else
-						harteldays[k] = 0;
-				}
-			}
-		}
-		strikedays = 0;
-		for (int l = 0; l < n; l++)
-			strikedays += harteldays[l];
-		cout << strikedays << endl;
-	}
-	return(0);
+  cin >> t;
+  if (t <= 0) exit(0);
+  
+  for (int i = 0; i < t; i++)
+  {
+    cin >> n;
+    cin >> p; 
+    harteldays = new int[n];
+    for (int j = 0; j < p; j++)
+    {
+      cin >> hi;
+      for (int k = 0; k < n; k++)
+      {
+        if ((k+1) % hi == 0)
+        {
+          if ((k+1) % 7 != 0 && (k+2) % 7 != 0)
+            harteldays[k] = 1;
+          else
+            harteldays[k] = 0;
+        }
+      }
+    }
+    strikedays = 0;
+    for (int l = 0; l < n; l++)
+      strikedays += harteldays[l];
+    cout << strikedays << endl;
+  }
+  return(0);
 }
