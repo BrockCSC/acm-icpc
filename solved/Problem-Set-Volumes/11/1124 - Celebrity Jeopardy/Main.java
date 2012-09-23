ddaba8cd736bed4201d9bb2e74e83f87dda9738f
Dennis Ideler <ideler.dennis@gmail.com>
2012-09-22 21:16:19 -0400

    Whoops... add the solution now

---
+++ solved/Problem-Set-Volumes/11/1124 - Celebrity Jeopardy/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main 
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    while (in.hasNext())
+    {
+      String line = in.nextLine();
+      System.out.println(line);
+    }
+  }
+}
