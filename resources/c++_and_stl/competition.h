b23cfe4a7e56301c11a7c33ce5654b9b7e9fc063
Dennis Ideler <ideler.dennis@gmail.com>
2013-03-11 17:06:43 -0300

    Update competition.h
    Add new #defines (as seen in Jakub Pachocki's contest code)

--- resources/c++_and_stl/competition.h
+++ resources/c++_and_stl/competition.h
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,16 +12,23 @@
 using namespace std;
 
 typedef long long               ll;
+typedef long double             ld;
 typedef vector<int>             vi;
 typedef vector<vi>              vvi;
-typedef pair<int, int>          pi;
+typedef pair<int, int>          pii;
 typedef vector<pi>              vii; // vector of integer pairs
 typedef set<int>                si;
 typedef map<string, int>        msi;
 
+#define VAR(a,b)        __typeof(b) a=(b)
 #define REP(i,n)        for(int i=0;i<(n);++i)     // 0 to n
-#define FOR(i,a,b)      for(int i=(a);i<=(b);++i)  // a to b, a < b
-#define FORD(i,a,b)     for(int i=(a);i>=(b);--i)  // a to b, a > b
+#define FOR(i,a,b)      for(VAR(i,a);i<=(b);++i)  // a to b, a < b
+#define FORD(i,a,b)     for(VAR(i,a);i>=(b);--i)  // a to b, a > b
+#define FORE(a,b)       for(VAR(a,(b).begin());a!=(b).end();++a) // for each, e.g. FORE(iter, vect) cout << *iter << endl;
+#define SIZE(a)         ((int)((a).size())) // e.g. for (int i = 0; i < SIZE(vect); ++i)
+#define ALL(x)          (x).begin(),(x).end() // e.g. sort(ALL(vect))
+#define FILL(x,a)       memset(x,a,sizeof(x))
+#define CLR(x)          memset(x,0,sizeof(x))
 #define VE              vector<int>
 #define SZ              size()
 #define PB              push_back
@@ -36,7 +44,6 @@ typedef map<string, int>        msi;
 #define remLastDigit(i) i/10
 #define char2int(c)     c-0
 #define char2idx(c)     c-'a'
-#define clr(arr)        memset(arr, 0, sizeof(arr));
 
 const double PI=acos(-1.0); //NOTES: PI
 const double EPS=1e-11; //NOTES: EPS
